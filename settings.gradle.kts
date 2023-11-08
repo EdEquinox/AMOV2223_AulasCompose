834b9649d2d1bbaf4cf34922cacf7149204df82d
EdEquinox <zemarquez97@hotmail.com>
2023-11-08 17:02:56 +0000

    aula1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AMOV2223_AulasCompose"
+include(":app")
+include(":app:aula1_sketches")
